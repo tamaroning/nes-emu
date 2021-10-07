634c231a3d4dc1a9892f60a830e37697de953b50
tamaron <tamaron1203@gmail.com>
2021-10-07 15:53:14 +0900

    add TAX, INX

--- src/cpu.rs
+++ src/cpu.rs
@@ -5,7 +5,18 @@ pub struct Cpu {
     pub a: u8,
     pub x: u8,
     pub y: u8,
-    pub stat: u8, // processor status
+    // processor status
+    /*
+        bit 7: negative (MSB of A)
+        bit 6: overflow
+        bit 5: hardwired 1 (not used)
+        bit 4: brk flag (6502 has two kinds of interrupt, BRK and IRQ)
+        bit 3: decimal (if set 1, run in BCD mode)
+        bit 2: interrupt flag (1: forbid interruption)
+        bit 1: zero
+        bit 0: carry
+    */
+    pub stat: u8,
 }
 
 impl Cpu {
@@ -20,35 +31,54 @@ impl Cpu {
         }
     }
 
+    fn update_zero_and_negative_flags(&mut self, result: u8) {
+        if result == 0 {
+            self.stat |= 0b0000_0010;
+        } else {
+            self.stat &= 0b1111_1101;
+        }
+
+        if result & 0b1000_0000 != 0 {
+            self.stat |= 0b1000_0000;
+        } else {
+            self.stat &= 0b0111_1111;
+        }
+    }
+
     pub fn interpret(&mut self, program: Vec<u8>) {
         loop {
             let opcode = program[self.pc as usize];
             self.pc += 1;
-            println!("{:X}", opcode);
+            println!("opcode: 0x{:X}", opcode);
 
             match opcode {
                 0x00 => { // BRK
                     return;
                 }
-                0xa9 => { // LDA imm
-                    let param = program[self.pc as usize];
+                0xA9 => { // LDA imm
+                    let imm = program[self.pc as usize];
                     self.pc += 1;
-                    self.a = param;
-
-                    if self.a == 0 {
-                        self.stat |= 0b0000_0010;
-                    } else {
-                        self.stat &= 0b1111_1101; 
-                    }
-
-                    if self.a & 0b1000_0000 != 0 {
-                        self.stat |= 0b1000_0000;
-                    } else {
-                        self.stat &= 0b0111_1111;
-                    }
-                }
+                    self.lda_imm(imm);
+                },
+                0xAA => self.tax(),
+                0xE8 => self.inx(),
                 _ => panic!("0x{:X} is not impremented", opcode),
             }
         }
+    }
+
+    fn lda_imm(&mut self, value: u8) {
+        self.a = value;
+        self.update_zero_and_negative_flags(self.a);
+    }
+  
+    fn tax(&mut self) {
+        self.x = self.a;
+        self.update_zero_and_negative_flags(self.x);
+    }
+
+    fn inx(&mut self) {
+        self.x = self.x.wrapping_add(1);
+        self.update_zero_and_negative_flags(self.x);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ mod test {
     use cpu::Cpu;
 
     #[test]
-    fn test_lda_imm() {
+    fn test_0xa9_lda_immidiate_load_data() {
         let mut cpu = Cpu::new();
         cpu.interpret(vec![0xa9, 0x05, 0x00]);
         assert!(cpu.stat & 0b0000_0010 == 0b00);
@@ -18,9 +18,35 @@ mod test {
     }
 
     #[test]
-    fn test_lda_imm_zero() {
+    fn test_0xa9_lda_zero_flag() {
         let mut cpu = Cpu::new();
         cpu.interpret(vec![0xa9, 0x00, 0x00]);
         assert!(cpu.stat & 0b0000_0010 == 0b10);
+    }
+
+   #[test]
+   fn test_0xaa_tax_move_a_to_x() {
+       let mut cpu = Cpu::new();
+       cpu.a = 10;
+       cpu.interpret(vec![0xaa, 0x00]);
+ 
+       assert_eq!(cpu.x, 10)
+   }
+
+   #[test]
+   fn test_5_ops_working_together() {
+       let mut cpu = Cpu::new();
+       cpu.interpret(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
+ 
+       assert_eq!(cpu.x, 0xc1)
+   }
+
+    #[test]
+    fn test_inx_overflow() {
+        let mut cpu = Cpu::new();
+        cpu.x = 0xff;
+        cpu.interpret(vec![0xe8, 0xe8, 0x00]);
+
+        assert_eq!(cpu.x, 1)
     }
 }
