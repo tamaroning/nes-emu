c56c8b97d548bfcaeac67c49ac3a9ef8088d6be6
tamaron <tamaron1203@gmail.com>
2021-10-13 23:45:43 +0900

    add sprite renderer

--- src/render/mod.rs
+++ src/render/mod.rs
@@ -19,11 +19,16 @@ fn bg_palette(ppu: &Ppu, tile_column: usize, tile_row: usize) -> [u8; 4] {
     [ppu.palette_table[0], ppu.palette_table[palette_start], ppu.palette_table[palette_start + 1], ppu.palette_table[palette_start + 2]]
 }
 
+fn sprite_palette(ppu: &Ppu, palette_idx: u8) -> [u8; 4] {
+    let start = 0x11 + (palette_idx * 4) as usize;
+    [0, ppu.palette_table[start], ppu.palette_table[start + 1], ppu.palette_table[start + 2]]
+}
+
 // TODO: Use appropriate palette
 pub fn render(ppu: &Ppu, frame: &mut frame::Frame) {
+    // draw background
     // two nametables exist
     let bank = ppu.ctrl.bkgnd_pattern_addr();
-
     // TODO: just for now, lets use the first nametable
     for i in 0 .. 0x3c0 {
         let tile = ppu.vram[i] as u16;
@@ -48,6 +53,50 @@ pub fn render(ppu: &Ppu, frame: &mut frame::Frame) {
                     _ => panic!(),
                 };
                 frame.set_pixel(tile_column * 8 + x, tile_row * 8 + y, rgb);
+            }
+        }
+    }
+    // draw sprites
+    for i in (0..ppu.oam_data.len()).step_by(4).rev() {
+        let tile_idx = ppu.oam_data[i + 1] as u16;
+        let tile_x = ppu.oam_data[i + 3] as usize;
+        let tile_y = ppu.oam_data[i] as usize;
+
+        let flip_vertical = if ppu.oam_data[i + 2] >> 7 & 1 == 1 {
+            true
+        } else {
+            false
+        };
+        let flip_horizontal = if ppu.oam_data[i + 2] >> 6 & 1 == 1 {
+            true
+        } else {
+            false
+        };
+        let palette_idx = ppu.oam_data[i + 2] & 0b11;
+        let sprite_palette = sprite_palette(ppu, palette_idx);
+        let bank: u16 = ppu.ctrl.sprite_pattern_addr();
+        let tile = &ppu.chr_rom[(bank + tile_idx * 16) as usize ..= (bank + tile_idx * 16 + 15) as usize];
+
+        for y in 0 ..= 7 {
+            let mut upper = tile[y];
+            let mut lower = tile[y + 8];
+            'xloop: for x in (0..=7).rev() {
+                let val = (1 & lower) << 1 | (1 & upper);
+                upper = upper >> 1;
+                lower = lower >> 1;
+                let rgb = match val {
+                    0 => continue 'xloop,
+                    1 => palette::SYSTEM_PALETTE[sprite_palette[1] as usize],
+                    2 => palette::SYSTEM_PALETTE[sprite_palette[2] as usize],
+                    3 => palette::SYSTEM_PALETTE[sprite_palette[3] as usize],
+                    _ => panic!(),
+                };
+                match (flip_horizontal, flip_vertical) {
+                    (false, false) => frame.set_pixel(tile_x + x, tile_y + y, rgb),
+                    (true, false) => frame.set_pixel(tile_x + 7 - x, tile_y + y, rgb),
+                    (false, true) => frame.set_pixel(tile_x + x, tile_y + 7 - y, rgb),
+                    (true, true) => frame.set_pixel(tile_x + 7 - x, tile_y + 7 - y, rgb),
+                }
             }
         }
     }
