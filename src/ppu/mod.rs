aa913a00478ed8750c26a282976a556d25d593bf
tamaron <tamaron1203@gmail.com>
2021-10-15 00:02:35 +0900

    add test

--- src/ppu/mod.rs
+++ src/ppu/mod.rs
@@ -244,4 +244,155 @@ pub mod test {
         assert_eq!(ppu.addr.get(), 0x2306);
         assert_eq!(ppu.read_data(), 0x66);
     }
+
+    #[test]
+    fn test_ppu_vram_reads_step_32() {
+        let mut ppu = Ppu::new_empty_rom();
+        ppu.write_to_ctrl(0b100);
+        ppu.vram[0x01ff] = 0x66;
+        ppu.vram[0x01ff + 32] = 0x77;
+        ppu.vram[0x01ff + 64] = 0x88;
+
+        ppu.write_to_ppu_addr(0x21);
+        ppu.write_to_ppu_addr(0xff);
+
+        ppu.read_data(); //load_into_buffer
+        assert_eq!(ppu.read_data(), 0x66);
+        assert_eq!(ppu.read_data(), 0x77);
+        assert_eq!(ppu.read_data(), 0x88);
+    }
+
+    #[test]
+    fn test_vram_horizontal_mirror() {
+        let mut ppu = Ppu::new_empty_rom();
+        ppu.write_to_ppu_addr(0x24);
+        ppu.write_to_ppu_addr(0x05);
+
+        ppu.write_to_data(0x66); //write to a
+
+        ppu.write_to_ppu_addr(0x28);
+        ppu.write_to_ppu_addr(0x05);
+
+        ppu.write_to_data(0x77); //write to B
+
+        ppu.write_to_ppu_addr(0x20);
+        ppu.write_to_ppu_addr(0x05);
+
+        ppu.read_data(); //load into buffer
+        assert_eq!(ppu.read_data(), 0x66); //read from A
+
+        ppu.write_to_ppu_addr(0x2C);
+        ppu.write_to_ppu_addr(0x05);
+
+        ppu.read_data(); //load into buffer
+        assert_eq!(ppu.read_data(), 0x77); //read from b
+    }
+
+    #[test]
+    fn test_vram_vertical_mirror() {
+        let mut ppu = Ppu::new(vec![0; 2048], Mirroring::Vertical);
+
+        ppu.write_to_ppu_addr(0x20);
+        ppu.write_to_ppu_addr(0x05);
+
+        ppu.write_to_data(0x66); //write to A
+
+        ppu.write_to_ppu_addr(0x2C);
+        ppu.write_to_ppu_addr(0x05);
+
+        ppu.write_to_data(0x77); //write to b
+
+        ppu.write_to_ppu_addr(0x28);
+        ppu.write_to_ppu_addr(0x05);
+
+        ppu.read_data(); //load into buffer
+        assert_eq!(ppu.read_data(), 0x66); //read from a
+
+        ppu.write_to_ppu_addr(0x24);
+        ppu.write_to_ppu_addr(0x05);
+
+        ppu.read_data(); //load into buffer
+        assert_eq!(ppu.read_data(), 0x77); //read from B
+    }
+
+    #[test]
+    fn test_read_status_resets_latch() {
+        let mut ppu = Ppu::new_empty_rom();
+        ppu.vram[0x0305] = 0x66;
+
+        ppu.write_to_ppu_addr(0x21);
+        ppu.write_to_ppu_addr(0x23);
+        ppu.write_to_ppu_addr(0x05);
+
+        ppu.read_data(); //load_into_buffer
+        assert_ne!(ppu.read_data(), 0x66);
+
+        ppu.read_status();
+
+        ppu.write_to_ppu_addr(0x23);
+        ppu.write_to_ppu_addr(0x05);
+
+        ppu.read_data(); //load_into_buffer
+        assert_eq!(ppu.read_data(), 0x66);
+    }
+
+    #[test]
+    fn test_ppu_vram_mirroring() {
+        let mut ppu = Ppu::new_empty_rom();
+        ppu.write_to_ctrl(0);
+        ppu.vram[0x0305] = 0x66;
+
+        ppu.write_to_ppu_addr(0x63); //0x6305 -> 0x2305
+        ppu.write_to_ppu_addr(0x05);
+
+        ppu.read_data(); //load into_buffer
+        assert_eq!(ppu.read_data(), 0x66);
+        // assert_eq!(ppu.addr.read(), 0x0306)
+    }
+
+    #[test]
+    fn test_read_status_resets_vblank() {
+        let mut ppu = Ppu::new_empty_rom();
+        ppu.stat.set_vblank_status(true);
+
+        let status = ppu.read_status();
+
+        assert_eq!(status >> 7, 1);
+        assert_eq!(ppu.stat.snapshot() >> 7, 0);
+    }
+
+    #[test]
+    fn test_oam_read_write() {
+        let mut ppu = Ppu::new_empty_rom();
+        ppu.write_to_oam_addr(0x10);
+        ppu.write_to_oam_data(0x66);
+        ppu.write_to_oam_data(0x77);
+
+        ppu.write_to_oam_addr(0x10);
+        assert_eq!(ppu.read_oam_data(), 0x66);
+
+        ppu.write_to_oam_addr(0x11);
+        assert_eq!(ppu.read_oam_data(), 0x77);
+    }
+
+    #[test]
+    fn test_oam_dma() {
+        let mut ppu = Ppu::new_empty_rom();
+
+        let mut data = [0x66; 256];
+        data[0] = 0x77;
+        data[255] = 0x88;
+
+        ppu.write_to_oam_addr(0x10);
+        ppu.write_oam_dma(&data);
+
+        ppu.write_to_oam_addr(0xf); //wrap around
+        assert_eq!(ppu.read_oam_data(), 0x88);
+
+        ppu.write_to_oam_addr(0x10);
+        ppu.write_to_oam_addr(0x77);
+        ppu.write_to_oam_addr(0x11);
+        ppu.write_to_oam_addr(0x66);
+    }
+
 }
